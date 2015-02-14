f0a2bfaebbb08b868b04216ef331eb0a617caab3
Hiroyuki Inoue <omega999kurahuto@yahoo.co.jp>
2015-02-14 16:25:20 +0900

    Initial Commit

---
+++ takikenApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  takikenApp
+//
+//  Created by 井上 裕之 on 2015/02/14.
+//  Copyright (c) 2015年 takiken. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
