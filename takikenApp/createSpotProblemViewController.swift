dde2554f97f02954295c9207fe68ef8e8899a853
Hiroyuki Inoue <omega999kurahuto@yahoo.co.jp>
2015-04-27 17:25:32 +0900

    deleted Pods files & add images

---
+++ takikenApp/createSpotProblemViewController.swift
@@ -0,0 +1,35 @@
+//
+//  createSpotProblemViewController.swift
+//  takikenApp
+//
+//  Created by 井上 裕之 on 2015/03/05.
+//  Copyright (c) 2015年 takiken. All rights reserved.
+//
+
+import UIKit
+
+class createSpotProblemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
