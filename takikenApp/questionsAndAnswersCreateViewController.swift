8423a10e8e71a9b3de5e0896c01602ddf562caf0
Hiroyuki Inoue <omega999kurahuto@yahoo.co.jp>
2015-06-03 19:48:23 +0900

    add 1問1答入力フォームの作成

--- takikenApp/questionsAndAnswersCreateViewController.swift
+++ takikenApp/questionsAndAnswersCreateViewController.swift
@@ -11,6 +11,12 @@ import UIKit
 class questionsAndAnswersCreateViewController: UIViewController {
     
     let appDelegate: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+    // *** 問題文textview ***
+    var _sentence = UITextView(frame: CGRectMake(30, 250, 310, 150))
+    // *** 答えtextview ***
+    var _answer = UITextView(frame: CGRectMake(30, 450, 310, 100))
+    // *** 解説文textview ***
+    var _description = UITextView(frame: CGRectMake(30, 600, 310, 200))
     
     @IBOutlet weak var backgroundImage: UIImageView!
     @IBAction func menuShowOnTap(sender: AnyObject) {
@@ -19,7 +25,7 @@ class questionsAndAnswersCreateViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         //スクリーンの幅
         let screenWidth = UIScreen.mainScreen().bounds.size.width;
@@ -30,16 +36,23 @@ class questionsAndAnswersCreateViewController: UIViewController {
         //背景画像のorigin
         let origin = backgroundImage.frame.origin
         
-        //CGRectで取得
-//        let myImage = UIImage(named: "kokuban.jpg")!
-//        var myImageView = UIImageView()
-//        myImageView.image = myImage
-//        myImageView.frame = CGRectMake(0, 64, screenWidth, screenHeight - 109)
-        //self.view.addSubview(myImageView)
+        // *** キーボード用の入力完了ボタンの作成 ***
+        var toolBar: UIToolbar!
+        toolBar = UIToolbar(frame: CGRectMake(0, self.view.frame.size.height/6, self.view.frame.size.width, 40.0))
+        toolBar.layer.position = CGPoint(x: self.view.frame.size.width/2, y: self.view.frame.size.height-20.0)
+        toolBar.barStyle = .BlackTranslucent
+        toolBar.tintColor = UIColor.whiteColor()
+        toolBar.backgroundColor = UIColor.blackColor()
+        
+        let toolBarBtn = UIBarButtonItem(title: "完了", style: .Done, target: self, action: "tappedToolBarBtn:")
+        toolBarBtn.tag = 1
+        toolBar.items = [toolBarBtn]
+        
+        // *** scrollViewの設定 ***
         var scrollView = UIScrollView()
         scrollView.frame =
             CGRectMake(origin.x, origin.y, imagePosition.width, imagePosition.height)
-        scrollView.contentSize = CGSizeMake(imagePosition.width, 1000)
+        scrollView.contentSize = CGSizeMake(imagePosition.width, 900)
         self.view.addSubview(scrollView)
         
         // *** タイトルラベル ***
@@ -69,13 +82,68 @@ class questionsAndAnswersCreateViewController: UIViewController {
         var icon = UIImageView(image: UIImage(named: "pen_mannenhitsu.png"))
         icon.frame = CGRectMake(290, 80, 70, 80)
         
+        // *** 問題文ラベル ***
+        var sentence_label = UILabel(frame: CGRectMake(30, 220, 100, 50))
+        sentence_label.text = "問題文"
+        sentence_label.textColor = UIColor.whiteColor()
+        sentence_label.font = UIFont.systemFontOfSize(22)
+        sentence_label.sizeToFit()
         
+        // *** 答えラベル ***
+        var answer_label = UILabel(frame: CGRectMake(30, 420, 100, 50))
+        answer_label.text = "答え"
+        answer_label.textColor = UIColor.whiteColor()
+        answer_label.font = UIFont.systemFontOfSize(22)
+        answer_label.sizeToFit()
         
+        // *** 解説文ラベル ***
+        var description_label = UILabel(frame: CGRectMake(30, 570, 100, 50))
+        description_label.text = "解説文"
+        description_label.textColor = UIColor.whiteColor()
+        description_label.font = UIFont.systemFontOfSize(22)
+        description_label.sizeToFit()
+        
+        // *** キーボード用編集完了ボタンの設置 ***
+        _sentence.inputAccessoryView = toolBar
+        _answer.inputAccessoryView = toolBar
+        _description.inputAccessoryView = toolBar
+        
+        // *** 投稿ボタン ***
+        var post_button = UIButton(frame: CGRectMake(33, 818, 300, 50))
+        var button_image = UIImage(named: "btn057_03.png")
+        //画像のリサイズ
+        let size = CGSize(width: 300, height: 60)
+        UIGraphicsBeginImageContext(size)
+        button_image!.drawInRect(CGRectMake(0,0, size.width, size.height))
+        var resizeImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        post_button.setBackgroundImage(resizeImage, forState: .Normal)
+        post_button.setTitle("問題を送信！", forState: .Normal)
+        post_button.setTitleColor(UIColor.blackColor(), forState: .Normal)
+        post_button.titleLabel?.font = UIFont.systemFontOfSize(22)
+        //ボタンをタップした時に実行するメソッドを指定
+//        button.addTarget(self, action: "tapped:", forControlEvents:.TouchUpInside)
+
         // *** scrollVIewへのaddSubview ***
         scrollView.addSubview(title)
         scrollView.addSubview(explanation)
         scrollView.addSubview(notice)
         scrollView.addSubview(icon)
+        scrollView.addSubview(_sentence)
+        scrollView.addSubview(_answer)
+        scrollView.addSubview(_description)
+        scrollView.addSubview(sentence_label)
+        scrollView.addSubview(answer_label)
+        scrollView.addSubview(description_label)
+        scrollView.addSubview(post_button)
+        
+    }
+    
+    // *** キーボード閉じる ***
+    func tappedToolBarBtn(sender: UIBarButtonItem) {
+        _sentence.resignFirstResponder()
+        _answer.resignFirstResponder()
+        _description.resignFirstResponder()
     }
     
     override func viewWillAppear(animated: Bool) {
