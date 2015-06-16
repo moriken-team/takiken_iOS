c88924667ac06d87b95119d5998a222c9f04736c
Hiroyuki Inoue <omega999kurahuto@yahoo.co.jp>
2015-06-16 19:01:27 +0900

    add 解答する問題が何問目であるかを把握するため，解答問題の通し番号となる変数を宣言

--- takikenApp/AppDelegate.swift
+++ takikenApp/AppDelegate.swift
@@ -21,6 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
     var answer:String?
     var rightAnswer:String?
     var rightDescription:String?
+    var answerProblemNumber:Int?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         GMSServices.provideAPIKey("AIzaSyBMtO1fNcnpd8HBam8mpg2wEDAQ_dYinC8")
