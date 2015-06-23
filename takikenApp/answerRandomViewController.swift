494c4b3b3cb6a9618fd19234d0d615582b4bfaa6
Hiroyuki Inoue <omega999kurahuto@yahoo.co.jp>
2015-06-23 18:47:00 +0900

    add ランダム問題を解答できるようにするため，セグエの設定と問題取得処理の追加

--- takikenApp/answerRandomViewController.swift
+++ takikenApp/answerRandomViewController.swift
@@ -11,7 +11,14 @@ import UIKit
 class answerRandomViewController: UIViewController {
     
     let _appDelegate: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+    let _problemModel: problemModel = problemModel()
 
+    @IBAction func challengeProbem(sender: AnyObject) {
+        let params:String = "kentei_id=1&public_flag=1&item=5"
+        _problemModel.setUpAnswer(params)
+        performSegueWithIdentifier("nextAnswer", sender: self)
+    }
+    
     @IBAction func showSideMenu(sender: AnyObject) {
         _appDelegate.slidingViewController?.anchorTopViewToRightAnimated(true)
     }
