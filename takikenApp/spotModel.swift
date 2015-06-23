ba1437d2cba74c3cd8a4bf26664bd2d9e70e48fb
Hiroyuki Inoue <omega999kurahuto@yahoo.co.jp>
2015-06-23 14:49:28 +0900

    create 地図に示すspot情報を管理するため，spotモデルを作成しスポット情報を抽出するメソッドを実装

---
+++ takikenApp/spotModel.swift
@@ -0,0 +1,32 @@
+//
+//  spotModel.swift
+//  takikenApp
+//
+//  Created by 井上 裕之 on 2015/06/22.
+//  Copyright (c) 2015年 takiken. All rights reserved.
+//
+
+import Foundation
+
+class spotModel {
+    let _api = accessToApi()
+    let _appDelegate: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+    
+    func index(params: String) -> NSMutableDictionary {
+        return _api.index("Spots", param: params)
+    }
+    
+    func fetchSpots() -> Array<AnyObject> {
+        let params: String = "kentei_id=5"
+        let apiResponse: NSMutableDictionary = self.index(params)
+        
+        // *** レスポンスデータからスポット情報の抽出 ***
+        let spotsMax: Int = apiResponse["response"]!["Spots"]!!.count
+        println(spotsMax)
+        var spots: Array<AnyObject> = []
+        for var i = 0; i < spotsMax; i++ {
+            spots.append(apiResponse["response"]!["Spots"]!![i]!["Spot"]!! as! Dictionary<String, String>)
+        }
+        return spots;
+    }
+}
