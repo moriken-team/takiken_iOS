0d1b40547d56ddfeff111ea57a11290bfba75488
Hiroyuki Inoue <omega999kurahuto@yahoo.co.jp>
2015-06-23 18:44:51 +0900

    add ランダム問題解答TOP画面からの画面遷移に関する設定

--- takikenApp/answerRandomViewController.swift
+++ takikenApp/answerRandomViewController.swift
@@ -9,11 +9,21 @@
 import UIKit
 
 class answerRandomViewController: UIViewController {
+    
+    let _appDelegate: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
 
+    @IBAction func showSideMenu(sender: AnyObject) {
+        _appDelegate.slidingViewController?.anchorTopViewToRightAnimated(true)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        self.view.addGestureRecognizer(_appDelegate.slidingViewController!.panGesture)
+        _appDelegate.slidingViewController?.anchorRightPeekAmount = 200.0
     }
 
     override func didReceiveMemoryWarning() {
