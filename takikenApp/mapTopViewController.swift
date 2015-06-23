4e63376b50b6d29cfc0f3b2069363ee9229fe1e4
Hiroyuki Inoue <omega999kurahuto@yahoo.co.jp>
2015-06-23 17:04:53 +0900

    add APIを利用した動的なスポット表示をするための処理を追加

--- takikenApp/mapTopViewController.swift
+++ takikenApp/mapTopViewController.swift
@@ -46,62 +46,35 @@ class mapTopViewController: UIViewController, GMSMapViewDelegate, CLLocationMana
         
         gpsLocationManager.startUpdatingLocation()
         /*** GPS利用の処理 end ***/
+        showGoogleMap()
     }
     
     func showGoogleMap() {
-        /*** GoogleMap利用の処理 start ***/
-        let lat: CLLocationDegrees = appDelegate.gpsLat
-        let lon: CLLocationDegrees = appDelegate.gpsLon
-        let lat2: CLLocationDegrees = 39.704340
-        let lon2: CLLocationDegrees = 141.137246
+        let spotApi: spotModel = spotModel()
+        let spots = spotApi.fetchSpots()
         
+        /*** GoogleMapへのスポット表示処理 start ***/
+        let initLat: CLLocationDegrees = appDelegate.gpsLat
+        let initLon: CLLocationDegrees = appDelegate.gpsLon
         let zoom: Float = 15
-        let camera: GMSCameraPosition = GMSCameraPosition.cameraWithLatitude(lat,longitude: lon,zoom: zoom);
-        let camera2: GMSCameraPosition = GMSCameraPosition.cameraWithLatitude(lat2,longitude: lon2,zoom: zoom);
-        let camera3: GMSCameraPosition = GMSCameraPosition.cameraWithLatitude(39.856134,longitude: 141.000232,zoom: zoom);
-        let camera4: GMSCameraPosition = GMSCameraPosition.cameraWithLatitude(39.775439,longitude: 140.991821,zoom: zoom);
-        let camera5: GMSCameraPosition = GMSCameraPosition.cameraWithLatitude(39.825423,longitude: 141.022892,zoom: zoom);
-        let gpsMarker: GMSMarker = GMSMarker()
-        let gpsMarker2: GMSMarker = GMSMarker()
-        let gpsMarker3: GMSMarker = GMSMarker()
-        let gpsMarker4: GMSMarker = GMSMarker()
-        let gpsMarker5: GMSMarker = GMSMarker()
-        
+        let initCamera: GMSCameraPosition = GMSCameraPosition.cameraWithLatitude(initLat,longitude: initLon,zoom: zoom);
         googleMap = GMSMapView(frame: CGRectMake(0, 0, self.view.bounds.width, self.view.bounds.height))
-        googleMap.camera = camera
+        googleMap.camera = initCamera
         googleMap.delegate = self
-        
-        self.view.addSubview(googleMap)
-        gpsMarker.position = camera.target
-        gpsMarker.title = "現在地"
-        //gpsMarker.snippet = "→詳細を見る←"
-        gpsMarker.appearAnimation = kGMSMarkerAnimationPop
-        gpsMarker.map = googleMap
-        
-        gpsMarker2.position = camera2.target
-        gpsMarker2.title = "旭橋"
-        gpsMarker2.snippet = "→詳細を見る←"
-        gpsMarker2.appearAnimation = kGMSMarkerAnimationPop
-        gpsMarker2.map = googleMap
-        
-        gpsMarker3.position = camera3.target
-        gpsMarker3.title = "岩手山"
-        gpsMarker3.snippet = "→詳細を見る←"
-        gpsMarker3.appearAnimation = kGMSMarkerAnimationPop
-        gpsMarker3.map = googleMap
-        
-        gpsMarker4.position = camera4.target
-        gpsMarker4.title = "小岩井農場"
-        gpsMarker4.snippet = "→詳細を見る←"
-        gpsMarker4.appearAnimation = kGMSMarkerAnimationPop
-        gpsMarker4.map = googleMap
-        
-        gpsMarker5.position = camera5.target
-        gpsMarker5.title = "鞍掛山"
-        gpsMarker5.snippet = "→詳細を見る←"
-        gpsMarker5.appearAnimation = kGMSMarkerAnimationPop
-        gpsMarker5.map = googleMap
-        /*** GoogleMap利用の処理 end ***/
+        let spotsMax = spots.count
+        for var i = 0; i < spotsMax; i++ {
+            let lat: CLLocationDegrees = (spots[i]["latitude"] as! NSString).doubleValue
+            let lon: CLLocationDegrees = (spots[i]["longitude"] as! NSString).doubleValue
+            let camera: GMSCameraPosition = GMSCameraPosition.cameraWithLatitude(lat,longitude: lon,zoom: zoom);
+            let marker: GMSMarker = GMSMarker()
+            self.view.addSubview(googleMap)
+            marker.position = camera.target
+            marker.title = spots[i]["name"] as! String
+            marker.snippet = "→詳細を見る←"
+            marker.appearAnimation = kGMSMarkerAnimationPop
+            marker.map = googleMap
+        }
+        /*** GoogleMapへのスポット表示処理 end ***/
     }
     
     //CLLocationManagerオブジェクトにデリゲートオブジェクトを設定すると初回に呼ばれるメソッド。gpsを有効にするため設定ダイアログを表示させるために使う
@@ -132,10 +105,10 @@ class mapTopViewController: UIViewController, GMSMapViewDelegate, CLLocationMana
         appDelegate.gpsLon = manager.location.coordinate.longitude
         
         //googleMap描写の処理
-        if checkGetGps == nil {
-            showGoogleMap()
+        //if checkGetGps == nil {
+            //showGoogleMap()
             checkGetGps = true
-        }
+        //}
         
         println("success")
         var location_log = String("\(manager.location.coordinate.latitude)") + ":" + String("\(manager.location.coordinate.longitude)")
